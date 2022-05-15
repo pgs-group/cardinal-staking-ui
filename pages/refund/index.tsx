218c8cb7e1fa74ad0d4459fa76a99c9aa0d9e724
mortezamohiuodin <mortezaMohiuodin@gmail.com>
2022-05-15 17:56:55 +0430

    chore : remove footer temparory

--- pages/refund/index.tsx
+++ pages/refund/index.tsx
@@ -72,7 +72,7 @@ export default function refund() {
             {loading && <Placeholder />}
           </div>
         </div>
-        <FooterRefund />
+        {/* <FooterRefund /> */}
       </div>
     </div>
   )
