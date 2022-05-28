c4d11d8edc4452f3e7957f2f7cb470f127abc95d
mortezamohiuodin <mortezaMohiuodin@gmail.com>
2022-05-28 11:09:39 +0430

    chore : add closeIcon to basicModal

--- components/ReleaseConfirmModal/index.tsx
+++ components/ReleaseConfirmModal/index.tsx
@@ -21,14 +21,8 @@ export default function ReleaseConfirmModal({ show, handleConfirm, onClose }) {
       closeModal={() => {
         onClose()
       }}
+      closeIcon
     >
-      <div className={styles.header}>
-        <span className={styles.closeButton} onClick={onClose}>
-          <svg width="14" height="14">
-            <path d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z"></path>
-          </svg>
-        </span>
-      </div>
       <div className={styles.wrapper}>
         <h3 className={styles.heading}>Are you sure?</h3>
         <p>
