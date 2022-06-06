0db51437f7ef606804414934c2b9a25f29151f92
Alireza Abdollahi <mra.abdollahi@gmail.com>
2022-06-06 17:24:15 +0430

    feat: tailwind container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     './rental-components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    /* #honeyland: change container size */
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1170px',
+      },
+    },
   },
   plugins: [],
 }
