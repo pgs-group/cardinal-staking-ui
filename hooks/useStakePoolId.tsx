2a8b509c34858f54c581cf9262f03c49d4ed0eec
Alireza Abdollahi <mra.abdollahi@gmail.com>
2022-06-06 18:20:40 +0430

    feat: add incubating page

--- hooks/useStakePoolId.tsx
+++ hooks/useStakePoolId.tsx
@@ -1,19 +1,22 @@
 import { tryPublicKey } from '@cardinal/namespaces-components'
 import { stakePoolMetadatas } from 'api/mapping'
 import { useRouter } from 'next/router'
+import { HONEYLAND_STAKE_POOL_ID } from '../honeyland/api/constants'
 
 export const useStakePoolId = () => {
+  let id: any = ''
+
   const {
     query: { stakePoolId },
   } = useRouter()
   const nameMapping = stakePoolMetadatas.find((p) => p.name === stakePoolId)
   const addressMapping = stakePoolMetadatas.find(
-    (p) => p.stakePoolAddress.toString() === stakePoolId
+    (p) => p.stakePoolAddress.toString() === id
   )
   const publicKey =
     nameMapping?.stakePoolAddress ||
     addressMapping?.stakePoolAddress ||
-    tryPublicKey(stakePoolId)
+    tryPublicKey(id)
 
   return publicKey
 }
